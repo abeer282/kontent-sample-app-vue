511c74f2880f0df0d2204ca327d9361bd0697688
abeer282 <abeerala282@gmail.com>
2020-07-26 13:52:19 +0300

    Add coffees filters test

--- e2e/cypress/components/products_catalog.spec.js
+++ e2e/cypress/components/products_catalog.spec.js
@@ -47,4 +47,24 @@ describe(`Product Catalog Testing`, () => {
         cy.get(`.store-menu-list > ul > :nth-child(2) > a`).click();
         cy.get(`.product-tile`).should(`have.length`, expectedBrewers);
     });
+
+    it(`should show the correct filters of coffees`, () => {
+        cy.get(`.store-menu-list > ul > :nth-child(1) > a`).click();
+        showsCoffeeProcessingFilters();
+        showsCoffeesStatusFilters();
+    });
+
+    const showsCoffeeProcessingFilters = () => {
+        cy.get(`.flex > .col-md-4 > :nth-child(1)`).should(`contain`, `Coffee processing`);
+        cy.get(`:nth-child(2) > .checkbox > label`).should(`contain`, `Wet (Washed)`); 
+        cy.get(`:nth-child(3) > .checkbox > label`).should(`contain`, `Dry (Natural)`); 
+        cy.get(`:nth-child(4) > .checkbox > label`).should(`contain`, `Semi-dry`);
+    };
+
+    const showsCoffeesStatusFilters = () => {
+        cy.get(`.col-md-4 > :nth-child(5)`).should(`contain`, `Status`);
+        cy.get(`:nth-child(6) > .checkbox > label`).should(`contain`, `On sale`); 
+        cy.get(`:nth-child(7) > .checkbox > label`).should(`contain`, `Bestseller`); 
+    };
+
 });
