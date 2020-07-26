de3387796f8c079e35bdcee5d8f07406dd7e6592
abeer282 <abeerala282@gmail.com>
2020-07-26 12:37:00 +0300

    Add article's elements test

--- e2e/cypress/components/articles.scpe.js
+++ e2e/cypress/components/articles.scpe.js
@@ -1,15 +1,17 @@
 describe(`Articles Testing`, () => {
+
+    let articleNumber = 5;
     
     beforeEach(() => {
         visitHomePage();
         passAdminConfigurations();
         enterArticlesPage();
     });
-    
+
     const visitHomePage = () => {
         cy.visit(`${Cypress.config().baseUrl}`);
     };
-    
+
     const passAdminConfigurations = () => {
         cy.get(`:nth-child(2) > .button-secondary`).click();
     };
@@ -26,5 +28,32 @@ describe(`Articles Testing`, () => {
         const expectedArticlesNum = 6;
         cy.get(`.article-tile`).should(`have.length`, expectedArticlesNum);
     });
+
+    it(`should show article's elements`, () => {
+        showElements(articleNumber);
+    });
+
+    const showElements = ((articleNum) => {
+        showTeaserImage(articleNum);
+        showDate(articleNum);
+        showTitle(articleNum);
+        showSummary(articleNum);
+    });
+
+    const showTeaserImage = (articleNum) => {
+        cy.get(`:nth-child(` + articleNum + `) > .col-md-3 > .article-tile > :nth-child(1) > .article-tile-image`);
+    };
+
+    const showDate = (articleNum) => {
+        cy.get(`:nth-child(` + articleNum + `) > .col-md-3 > .article-tile > .article-tile-date`);
+    };
+
+    const showTitle = (articleNum) => {
+        cy.get(`:nth-child(` + articleNum + `) > .col-md-3 > .article-tile > .article-tile-content > .h4 > a`);
+    };
+
+    const showSummary = (articleNum) => {
+        cy.get(`:nth-child(` + articleNum + `) > .col-md-3 > .article-tile > .article-tile-content > .article-tile-text`);
+    };
 
 });
