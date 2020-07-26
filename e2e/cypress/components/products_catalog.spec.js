01274d3f967ce727e00df170276710be9bf43caf
abeer282 <abeerala282@gmail.com>
2020-07-26 13:38:11 +0300

    Add products menu test

--- e2e/cypress/components/products_catalog.spec.js
+++ e2e/cypress/components/products_catalog.spec.js
@@ -23,4 +23,17 @@ describe(`Product Catalog Testing`, () => {
         cy.title().should(`eq`, `Dancing Goat`);
     });
 
+    it(`should show the correct menu bar`, () => {
+        showsCoffees();
+        showsBrewers();
+    });
+
+    const showsCoffees = () => {
+        cy.get(`.store-menu-list > ul > :nth-child(1) > a`).should(`contain`, `Coffees`);
+    };
+
+    const showsBrewers = () => {
+        cy.get(`.store-menu-list > ul > :nth-child(2) > a`).should(`contain`, `Brewers`);
+    };
+
 });
