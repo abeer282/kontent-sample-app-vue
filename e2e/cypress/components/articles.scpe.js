e98593be1aa1f68bb2eb5ebb74d3f9da438b802e
abeer282 <abeerala282@gmail.com>
2020-07-26 12:10:32 +0300

    Add before each

--- e2e/cypress/components/articles.scpe.js
+++ e2e/cypress/components/articles.scpe.js
@@ -1,9 +1,24 @@
 describe(`Articles Testing`, () => {
+    
+    beforeEach(() => {
+        visitHomePage();
+        passAdminConfigurations();
+        enterArticlesPage();
+    });
+    
+    const visitHomePage = () => {
+        cy.visit(`${Cypress.config().baseUrl}`);
+    };
+    
+    const passAdminConfigurations = () => {
+        cy.get(`:nth-child(2) > .button-secondary`).click();
+    };
+
+    const enterArticlesPage = () => {
+        cy.get(`:nth-child(3) > a`).click();
+    };
 
     it(`should show the correct page title`, () => {
-        cy.visit(`${Cypress.config().baseUrl}`);
-        cy.get(`:nth-child(2) > .button-secondary`).click();
-        cy.get(`:nth-child(3) > a`).click();
         cy.title().should(`eq`, `Dancing Goat`);
     });
 
